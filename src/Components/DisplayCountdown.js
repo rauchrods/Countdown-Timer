e5eeed8c1b340f1556e014a7911ce780c777b844
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2023-12-26 13:17:39 +0530

    updated date

--- src/Components/DisplayCountdown.js
+++ src/Components/DisplayCountdown.js
@@ -7,9 +7,9 @@ function DisplayCountdown() {
     return (
         <div className="countdown-container">
             <MyAudio/>
-            <Clock target = {"09 November 2023 12:00 Am"} title={"Richard's Birthday in..."}/>
-            <Clock target = {"24 December 2023 12:00 Am"} title={"Roysten's Birthday in..."}/>
-            <Clock target = {"25 December 2023 12:00 Am"} title={"Christmas Countdown..."}/>
+            <Clock target = {"09 November 2024 12:00 Am"} title={"Richard's Birthday in..."}/>
+            <Clock target = {"24 December 2024 12:00 Am"} title={"Roysten's Birthday in..."}/>
+            <Clock target = {"25 December 2024 12:00 Am"} title={"Christmas Countdown..."}/>
             <Clock target = {"27 January 2024 12:00 Am"} title={"Roysten's Wedding in..."}/>
             {/* <img src={snowfall05} alt="snaow fall gif" class="snow-gif" /> */}
             
